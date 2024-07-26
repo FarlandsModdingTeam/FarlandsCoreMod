93c74fdadafae706d619fc29dc25c10e50079d95
MagincyanGames <77320870+MagincyanGames@users.noreply.github.com>
2024-07-26 15:29:49 +0200

    Create Structure

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace FarlandsEmptyMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class YourPlugin: FarlandsCoreMod.FarlandsMod
+    {
+        public override string SHORT_NAME => "YourShortName";
+
+        public override void OnStart()
+        {
+            
+        }
+    }
+}
