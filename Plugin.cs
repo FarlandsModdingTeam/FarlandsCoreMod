69049a34529e805de6f8dd0c0fa6faff6ea0c113
MagincyanGames <77320870+MagincyanGames@users.noreply.github.com>
2024-07-28 11:29:18 +0200

    update

--- Plugin.cs
+++ Plugin.cs
@@ -8,11 +8,13 @@ namespace FarlandsEmptyMod
     {
         public override string SHORT_NAME => "YourShortName";
 
+        // Se ejecuta cuando carga el mod
         public override void OnStart()
         {
             
         }
 
+        // Se ejecuta cuando carga el juego
         public override void OnFirstFrame()
         {
             
