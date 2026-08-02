d796852f7e0eb0f3f6b13845e07cd936e4111986
CarlosPalmaSantos <carlospalmasantos4@gmail.com>
2026-08-02 11:14:12 +0200

    chore: Initializing Project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FarlandsCoreMod
-Library to simplify farlands modding 
+
+A library designed to simplify Farlands modding.
+
+## Development
+
+If you want to build the project, create the `lib/game` directory and copy `Assembly-CSharp.dll` and `Assembly-CSharp-firstpass.dll` into it.
+
+These files are not provided in this repository. You must extract them from your local copy of **Farlands**.
---
+++ src/FarlandsCoreMod/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace FarlandsCoreMod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class FCMPlugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+    }
+}
